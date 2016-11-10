ff1d28474f78eec6d36e4badb310d7d9eaca8dd9
pkgit <karim.patera@onebip.com>
2016-11-10 11:16:43 +0000

    changed all configuration from CET to UTC

--- spec/unit/Onebip/DateTime/UTCDateTimeTest.php
+++ spec/unit/Onebip/DateTime/UTCDateTimeTest.php
@@ -42,7 +42,7 @@ class UTCDateTimeTest extends PHPUnit_Framework_TestCase
         $date = new DateTime();
         $dateTime = UTCDateTime::box($date);
 
-        $output = $dateTime->toDateTime(new DateTimeZone("Europe/Rome"));
+        $output = $dateTime->toDateTime(new DateTimeZone("UTC"));
         $this->assertEquals($date->getTimestamp(), $output->getTimestamp());
         $this->assertEquals($date, $output);
     }
